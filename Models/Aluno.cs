839a7d18f957f625872860385371a006b2d23e0d
Matheus-S-Costa <matheusilva334@gmail.com>
2022-12-13 20:03:52 -0300

    edit obj

--- Models/Aluno.cs
+++ Models/Aluno.cs
@@ -5,5 +5,14 @@ public class Aluno
     public int Id {get; set;}
 
     public string? Nome {get; set;}
-    public double Nota{get; set;}
+    public string? Email {get; set;}
+    public string? Senha {get; set;}
+    public string? Nascimento {get; set;}
+    public string? Cpf {get; set;}
+    public string? Telefone {get; set;}
+    public int Ra {get; set;}
+    public string? Faculdade {get; set;}
+    public string? Curso {get; set;}
+    public string? Instituicao1 {get; set;}
+    public string? Instituicao2 {get; set;}
 }
