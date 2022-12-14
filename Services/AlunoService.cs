312c00a310135715fb0cbd47e09fc898ea03985d
Matheus-S-Costa <matheusilva334@gmail.com>
2022-12-13 22:57:22 -0300

    edit cadastro

--- Services/AlunoService.cs
+++ Services/AlunoService.cs
@@ -12,7 +12,7 @@ public static class AlunoService
             Alunos = new List<Aluno>
             {
                 new Aluno { Id = 1, Nome = "Ana", Email="refused@teste.com", Senha="teste", Nascimento="04/05/2000", Cpf="000000000-00", Telefone="(31)00000-0000", Ra= 320137871, Faculdade="Una", Curso="Gastronomia", Instituicao1="Ulisboa-PORT", Instituicao2="Eduportugal-PORT"},
-                new Aluno { Id = 1, Nome = "Bia", Email="teste@example.com", Senha="teste2", Nascimento="01/01/2000", Cpf="000000000-11", Telefone="(31)94000-0000", Ra= 320137870, Faculdade="Una", Curso="Psicologia", Instituicao1="Ulisboa", Instituicao2="Eduportugal"}
+                new Aluno { Id = 2, Nome = "Bia", Email="teste@example.com", Senha="teste2", Nascimento="01/01/2000", Cpf="000000000-11", Telefone="(31)94000-0000", Ra= 320137870, Faculdade="Una", Curso="Psicologia", Instituicao1="Ulisboa", Instituicao2="Eduportugal"}
             };
         }
     public static List<Aluno> GetAll() => Alunos;
